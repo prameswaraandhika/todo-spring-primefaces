4db924741b8527baec6552000755d5fce4156837
Andhikap <prameswaara@gmail.com>
2024-01-23 00:38:53 +0700

    create service save todo

--- src/main/java/com/prameswaradev/todospringprimefaces/controller/IndexController.java
+++ src/main/java/com/prameswaradev/todospringprimefaces/controller/IndexController.java
@@ -3,10 +3,13 @@ package com.prameswaradev.todospringprimefaces.controller;
 import com.prameswaradev.todospringprimefaces.model.Todo;
 import com.prameswaradev.todospringprimefaces.service.TodoService;
 import jakarta.annotation.PostConstruct;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.primefaces.PrimeFaces;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -38,7 +41,23 @@ public class IndexController {
     }
 
     public void saveTodo(){
+        log.info("Todo has been saved: " + this.selectedTodo);
+        // Add
+        if (this.selectedTodo.getId() == null) {
+            this.todoService.createTodo(this.selectedTodo);
+            this.todos.add(this.selectedTodo);
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage("Todo Added"));
+        } else { // Update
+            this.todoService.createTodo(this.selectedTodo);
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage("Todo Updated"));
+        }
+        PrimeFaces.current().executeScript("PF('todoModalWindow').hide()");
+        PrimeFaces.current().ajax().update("form-todo:messages",
+                "form-todo:todo-table");
 
+        this.selectedTodo = null;
     }
     public void deleteTodo(){
 
